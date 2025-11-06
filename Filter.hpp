9757ce140f4652009ec751bf0e7dac80c27d6451
Андрій <andrii.lishchynskyi.ap.2024@lpnu.ua>
2025-11-06 22:30:17 +0200

    Update Filter.hpp

--- Filter.hpp
+++ Filter.hpp
@@ -5,19 +5,24 @@ class Filter {
 protected:
     int order;
     std::string type;
+    double minTemp;
+    double maxTemp;
 
 public:
     Filter();
-    Filter(int order, const std::string& type);
+    Filter(int order, const std::string& type, double minTemp, double maxTemp);
     virtual ~Filter();
 
     bool setOrder(int order);
     bool setType(const std::string& type);
+    bool setMinTemp(double t);
+    bool setMaxTemp(double t);
 
     int getOrder() const;
     std::string getType() const;
+    double getMinTemp() const;
+    double getMaxTemp() const;
 
     virtual void input();
     virtual void print() const;
 };
-
